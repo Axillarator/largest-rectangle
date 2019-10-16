d799bf594aba4339b06f9164a70835cabcff5cce
ag <axel.gruenert@merkkur.de>
2019-10-16 10:56:34 +0200

    project setup with first tests

---
+++ src/test/kotlin/MainTest.kt
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+internal class MainTest{
+
+    @Test
+    fun runTests(){
+        assertEquals(2, 2)
+    }
+
+    @Test
+    fun runTests2(){
+        assertEquals(2, 3)
+    }
+}
